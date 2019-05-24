4e45f06b7724cb2277a53888b1a4fdbc8384c2ca
twhughes <tylerwhughes91@gmail.com>
2019-05-24 16:18:24 -0700

    fixed box demo

--- examples/box.py
+++ examples/box.py
@@ -15,16 +15,16 @@ PLOT = False
 # make parameters
 omega = 2*np.pi*200e12
 L0 = 1e-5
-Nx, Ny = 110, 110
-eps_max = 5
-eps_r = np.ones((Nx, Ny))
+eps_max = 2
 npml = 10
 spc = 10
 L = 50
+Nx, Ny = 2*npml + 4*spc + L, 2*npml + 4*spc + L
+eps_r = np.ones((Nx, Ny))
 
 # make source
 source = np.zeros((Nx, Ny))
-source[npml+spc, Ny//2] = 1e1
+source[npml+spc, Ny//2] = 1
 
 # make design region
 box_region = np.zeros((Nx, Ny))
@@ -43,7 +43,7 @@ if PLOT:
 F = fdfd_hz(omega, L0, eps_r, source, [npml, npml])
 Ex, Ey, Hz = F.solve()
 E_mag = np.sqrt(np.square(np.abs(Ex)) + np.square(np.abs(Ey)))
-H_mag = np.sqrt(np.square(np.abs(Hz)))
+H_mag = np.abs(Hz)
 I_E0 = np.abs(np.square(np.sum(E_mag * probe)))
 I_H0 = np.abs(np.square(np.sum(H_mag * probe)))
 
@@ -77,7 +77,7 @@ def intensity(eps_arr):
     Ex, Ey, Hz = F.solve()
 
     # compute the gradient and normalize if you want
-    I = npa.sum(npa.abs(Hz * probe))
+    I = npa.sum(npa.square(npa.abs(Hz * probe)))
     return -I / I_H0
 
 # define the gradient for autograd
@@ -98,17 +98,18 @@ from scipy.optimize import minimize
 bounds = [(1, eps_max) if box_region.flatten()[i] == 1 else (1,1) for i in range(eps_r.size)]
 minimize(intensity, eps_r, args=(), method='L-BFGS-B', jac=grad_I,
     bounds=bounds, tol=None, callback=None,
-    options={'disp': True, 'maxcor': 10, 'ftol': 2.220446049250313e-09, 'gtol': 1e-05, 'eps': 1e-08, 'maxfun': 15000, 'maxiter': 15000, 'iprint': -1, 'maxls': 20})
+    options={'disp': True, 'maxcor': 10, 'ftol': 2.220446049250313e-09, 'gtol': 1e-05, 'eps': 1e-08, 'maxfun': 15000, 'maxiter': 150, 'iprint': -1, 'maxls': 20})
 
 # plot the final permittivity
-plt.imshow(F.eps_r._value, cmap='nipy_spectral')
+plt.imshow(F.eps_r._value.T, cmap='nipy_spectral')
 plt.colorbar()
 plt.show()
 
 Ex, Ey, Hz = F.solve()
-plt.imshow(np.real(Hz._value), cmap='RdBu')
+plt.imshow(np.real(Hz._value).T, cmap='RdBu')
 plt.title('H_z / E0 (<-)')
 plt.xlabel('y')
 plt.ylabel('x')
 plt.colorbar()
 plt.show()
+
