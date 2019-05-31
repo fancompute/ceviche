7f7746c18edbb9061fa2b0a3e5daf9dba66e8506
twhughes <tylerwhughes91@gmail.com>
2019-05-31 12:05:00 -0700

    refined and added comments to multi-wavelength focusing example

--- examples/multi_wavelength_focus.py
+++ examples/multi_wavelength_focus.py
@@ -1,6 +1,7 @@
 import autograd.numpy as np
 import matplotlib.pylab as plt
 
+from scipy.optimize import minimize
 from autograd import grad
 
 from ceviche import fdfd_hz
@@ -8,93 +9,136 @@ from ceviche.constants import C_0
 
 PLOT = True
 
-# some parameters
-wavelengths = [500e-9, 450e-9]#, 650e-9]
+""" define parameters """
 
-H = 2e-6  # height of slab
-L = 2e-6  # width of slab
+wavelengths = [450e-9, 550e-9, 650e-9]  # in meters
+Nw = len(wavelengths)                   # number of wavelengths
 
-spc = 1e-6   # space between source and PML, source and structure
-dL = 20e-9
+H = 1e-6           # height of slab
+L = 1e-6           # width of slab
 
-npml = 20       # number of PML grids
-eps_max = 15   # material index
+spc = 2e-6         # space between source and PML, source and structure
+dL = 20e-9         # size (meters) of each grid cell in FDFD
 
-# setup arrays
-Nx = int(L / dL)
-Ny = 2*npml + 2*int(spc / dL) + int(H / dL)
-x_pts = dL * np.arange(Nx)
+npml = 10          # number of PML grids
+eps_max = 12       # material index
 
-slab_region = np.zeros((Nx, Ny))
-slab_region[:, npml + int(spc / dL):npml + int(spc / dL) + int(H / dL)] = 1
+""" set up arrays for this problem (note, need to convert from meters to grid cells using dL) """
 
+Nx = int(L / dL)                              # number of grid cells in x
+Ny = 2*npml + 2*int(spc / dL) + int(H / dL)   # number of grid cells in y
+
+# defines where the slab / design region will be (where this array = 1)
+design_region = np.zeros((Nx, Ny))
+design_region[:, npml + int(spc / dL):npml + int(spc / dL) + int(H / dL)] = 1
+
+# the starting relative permittivity
 eps_r = np.ones((Nx, Ny))
 
+# defines where the source will be (plane wave)
 source = np.zeros((Nx, Ny))
 source[:, npml + int(spc / 2 / dL)] = 1
 
+# defines the y_index of the measurement points
 probe_index_y = npml + int(3 * spc / 2 / dL) + int(H / dL)
 
-probe1 = np.zeros((Nx, Ny))
-probe1[Nx//3, probe_index_y] = 1
-probe2 = np.zeros((Nx, Ny))
-probe2[2*Nx//3, probe_index_y] = 1
+# make a list of probe locations, evenly spaced in x and at probe_index_y
+probes = []
+space_x = Nx / float(Nw)                  # even spacing between probes
+for i in range(Nw):
+    index_i = int(space_x * (1/2 + i))    # the x index of the ith probe
+    probe_i = np.zeros((Nx, Ny))          # ith probe
+    probe_i[index_i, probe_index_y] = 1   # define at this point
+    probes.append(probe_i)                # add to list
 
-omega1 = 2 * np.pi * C_0 / wavelengths[0]
-fdfd1 = fdfd_hz(omega1, dL, eps_r, source, npml=[0, npml])
-Ex, Ey, Hz = fdfd1.solve()
-P1 = np.sum(np.abs(Hz) * probe1)
+# make a list of FDFDs at each wavelength and their power normalizations
+fdfds = []
+powers = []
+for i, lam in enumerate(wavelengths):
+    omega_i = 2 * np.pi * C_0 / lam      # the angular frequency
+    fdfd_i = fdfd_hz(omega_i, dL, eps_r, source, npml=[0, npml])   # make an FDFD simulation
+    fdfds.append(fdfd_i)                                           # add it to the list
+    Ex, Ey, Hz = fdfd_i.solve()                                    # solve the fields
+    powers.append(np.sum(np.square(np.abs(Hz) * probes[i])))       # compute the power at its probe, add to list
 
-omega2 = 2 * np.pi * C_0 / wavelengths[1]
-fdfd2 = fdfd_hz(omega2, dL, eps_r, source, npml=[0, npml])
-Ex, Ey, Hz = fdfd2.solve()
-P2 = np.sum(np.abs(Hz) * probe2)
-
+# plot the domain (design region + probes + source)
 if PLOT:
-    plt.imshow((probe1 + probe2 + source + slab_region).T)
+    plt.imshow((sum(probes) + source + design_region).T, cmap='Greys')
+    plt.title('domain')
+    plt.xlabel('x')
+    plt.ylabel('y')   
+    plt.colorbar()     
     plt.show()
 
+# plots the real part of Hz
 def plot_field(fdfd):
     Ex, Ey, Hz = fdfd.solve()
-    plt.imshow(np.real(Hz))
+    plt.imshow(np.real(Hz.T), cmap='RdBu')
+    plt.title('real(Hz)')
+    plt.xlabel('x')
+    plt.ylabel('y')   
+    plt.colorbar() 
     plt.show()
 
+# plots the real part of Hz for the autograd object (just needs to be called instead of the `plot_field` after the optimization)
 def plot_field_ag(fdfd):
     Ex, Ey, Hz = fdfd.solve()
-    plt.imshow(np.abs(Hz._value))
+    plt.imshow(np.real(Hz._value.T), cmap='RdBu')
+    plt.title('real(Hz)')
+    plt.xlabel('x')
+    plt.ylabel('y')    
+    plt.colorbar() 
     plt.show()
 
-plot_field(fdfd1)
-plot_field(fdfd2)
+# plot each of the starting fields
+if PLOT:
+    for i in range(Nw):        
+        plot_field(fdfds[i])
 
+""" Objective function """
+
+# the objective function (to minimize) as a function of the (flattened) permittivity array
 def objective(eps_arr):
 
+    # reshape flattened array into the right grid shape
     eps_r = eps_arr.reshape((Nx, Ny))
 
-    fdfd1.eps_r = eps_r
-    Ex, Ey, Hz1 = fdfd1.solve()
-    power1_1 = np.abs(np.sum(probe1 * Hz1))
-    power1_2 = np.abs(np.sum(probe2 * Hz1))
+    # running sum of the power at each probe for each wavelength
+    power_total = 0.0
 
-    fdfd2.eps_r = eps_r
-    Ex, Ey, Hz2 = fdfd2.solve()
-    power2_1 = np.abs(np.sum(probe1 * Hz2))
-    power2_2 = np.abs(np.sum(probe2 * Hz2))
+    # looop through wavelengths, fdfds, probes
+    for i in range(Nw):
+        fdfds[i].eps_r = eps_r           # set the relative permittivity of the FDFD
+        Ex, Ey, Hz = fdfds[i].solve()    # solve the fields
+        power = np.abs(np.square(np.sum(probes[i] * Hz))) / powers[i]   # compute the power at the probe
+        power_total += power             # add to sum
 
-    return -power1_1 / P1 - power2_2 / P2# + power1_2 / P1 + power2_1 / P2
+    return - power_total / Nw            # return negative (for maximizing) and normalize by number of wavelengths (starting objective = 1)
 
 # define the gradient for autograd
 grad_J = grad(objective)
 
-# optimization loop
-NIter = 100
-from scipy.optimize import minimize
-bounds = [(1, eps_max) if slab_region.flatten()[i] == 1 else (1,1) for i in range(eps_r.size)]
+"""  optimization loop """
+
+NIter = 10000    # max number of iterations
+
+# set the material bounds to (1, eps_max) in design region, (1,1) outside
+bounds = [(1, eps_max) if design_region.flatten()[i] == 1 else (1,1) for i in range(eps_r.size)]  
+
+# call a constrained optimization function from Scipy (progress prints to terminal)
 minimize(objective, eps_r, args=(), method='L-BFGS-B', jac=grad_J,
     bounds=bounds, tol=None, callback=None,
     options={'disp': True, 'maxcor': 10, 'ftol': 2.220446049250313e-09, 'gtol': 1e-05, 'eps': 1e-08, 'maxfun': 15000, 'maxiter': NIter, 'iprint': -1, 'maxls': 20})
 
+# plot the fields of the final FDFDs
+if PLOT:
+    for i in range(Nw):        
+        plot_field_ag(fdfds[i])
 
-
-plot_field_ag(fdfd1)
-plot_field_ag(fdfd2)
+# plot the final permittivity
+plt.imshow(fdfds[0].eps_r._value.T, cmap='Greys')
+plt.title('final relative permittivity')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.colorbar()
+plt.show()
