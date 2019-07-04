Fixed sigmoid; moved numerical vjp maker

ceviche/parameterizations.py
@@ -1,5 +1,6 @@
 import autograd.numpy as np
 from autograd.extend import primitive, defvjp
+import numpy
 
 from ceviche.utils import circ2eps, grid_coords
 from ceviche.primitives import vjp_maker_num
@@ -47,8 +48,10 @@ class Param_Shape(Param_Base):
     @staticmethod
     def sigmoid(x, strength=.1):
         # used to anti-alias the circle, higher strength means sharper boundary
-        return np.exp(x * strength) / (1 + np.exp(x * strength))
-
+        x1 = x * (x >= 0)
+        x2 = x * (x < 0)
+        return 1 / (1 + np.exp(-x1 * strength)) + \
+            np.exp(x2 * strength) / (1 + np.exp(x2 * strength)) - 1/2
 
 class Circle_Shapes(Param_Shape):
 
@@ -60,8 +63,8 @@ class Circle_Shapes(Param_Shape):
 
     def circle(self, xs, ys, x, y, r):
         # defines an anti aliased circle    
-        dist_from_edge = (xs - x)**2 + (ys - y)**2 - r**2
-        return self.sigmoid(-dist_from_edge / self.dL**2)
+        dist_from_edge = np.power((xs - x), 2) + np.power((ys - y), 2) - np.power(r, 2)
+        return self.sigmoid(-dist_from_edge / np.power(self.dL, 2))
 
     def get_eps(self, xs, ys, rs, values):
         # returns the permittivity array for a bunch of holes at positions (xs, ys) with radii rs and epsilon values (val)

examples/box_circles.py
@@ -93,24 +93,24 @@ def intensity(params):
 
     # compute the gradient and normalize if you want
     I = npa.sum(npa.square(npa.abs(Hz * probe)))
-    return -I / I_H0
+    return -I / I_H0 / 1e6
 
 # define the gradient for autograd
 grad_I = grad(intensity)
 
-from ceviche.optimizers import adam_minimize
-# bounds = [(1, eps_max) if box_region.flatten()[i] == 1 else (1,1) for i in range(eps_r.size)]
-of_list = adam_minimize(intensity, params.ravel(), jac=grad_I, step_size=dL/4, Nsteps=100,
-    bounds=None, options={'disp': True})
-plt.plot(-np.array(of_list))
-plt.show()
-
-# from scipy.optimize import minimize
+# from ceviche.optimizers import adam_minimize
 # # bounds = [(1, eps_max) if box_region.flatten()[i] == 1 else (1,1) for i in range(eps_r.size)]
-# minimize(intensity, params.ravel(), args=(), method='L-BFGS-B', jac=grad_I,
-#     bounds=None, tol=None, callback=None,
-#     options={'disp': True, 'maxcor': 10, 'ftol': 2.220446049250313e-09, 'gtol': 1e-05, 
-#     'eps': 1e-08, 'maxfun': 15000, 'maxiter': 10, 'iprint': -1, 'maxls': 18})
+# of_list = adam_minimize(intensity, params.ravel(), jac=grad_I, step_size=dL/4, Nsteps=100,
+#     bounds=None, options={'disp': True})
+# plt.plot(-np.array(of_list))
+# plt.show()
+
+from scipy.optimize import minimize
+# bounds = [(1, eps_max) if box_region.flatten()[i] == 1 else (1,1) for i in range(eps_r.size)]
+minimize(intensity, params.ravel(), args=(), method='L-BFGS-B', jac=grad_I,
+    bounds=None, tol=None, callback=None,
+    options={'disp': True, 'maxcor': 10, 'ftol': 2.220446049250313e-09, 'gtol': 1e-05, 
+    'eps': 1e-08, 'maxfun': 15000, 'maxiter': 10, 'iprint': -1, 'maxls': 18})
 
 
 # plot the final permittivity