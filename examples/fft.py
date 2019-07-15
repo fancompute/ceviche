3aac0e056f49dc6b23dd83a1b2de065130753778
twhughes <tylerwhughes91@gmail.com>
2019-07-14 17:50:36 -0700

    jvp for fft

--- examples/fft.py
+++ examples/fft.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import
 import sys
 sys.path.append('../ceviche')
 
@@ -9,19 +10,36 @@ from autograd.extend import defjvp
 from scipy.linalg import dft
 import matplotlib.pylab as plt
 
-# def fft_grad(get_args, fft_fun, ans, x, *args, **kwargs):
-#     axes, s, norm = get_args(x, *args, **kwargs)
-#     check_no_repeated_axes(axes)
-#     vs = vspace(x)
-#     return lambda g: match_complex(x, truncate_pad(fft_fun(g, *args, **kwargs), vs.shape))
+from autograd.extend import primitive, defjvp
 
-# defvjp(fft, lambda *args, **kwargs:
-#         fft_grad(get_fft_args, fft, *args, **kwargs))
+@primitive
+def my_fft(x):    
+    """ 
+    Wrapper for numpy's FFT, so I can add a primitive to it
+        FFT(x) is like a DFT matrix (D) dot with x
+    """
+    return np.fft.fft(x)
+# my_fft = primitive(np.fft.fft)
 
+def fft_grad(g, ans, x):
+    """ 
+    Define the jacobian-vector product of my_fft(x)
+        The gradient of FFT times g is the vjp
+        ans = fft(x) = D @ x
+        jvp(fft(x))(g) = d{fft}/d{x} @ g
+                       = D @ g
+        Therefore, it looks like the FFT of g
+    """
+    return np.fft.fft(g)
+
+defjvp(my_fft, fft_grad)
+
+###
 
 Nx = 50
 Ny = 50
 Nz = 1
+
 
 npml = 10
 
@@ -31,7 +49,7 @@ pml = [npml, npml, 0]
 
 # source parameters
 sigma = 10e-15
-total_time = 0.4e-12
+total_time = 0.5e-12
 t0 = sigma * 10
 
 source_amp = 1
@@ -41,15 +59,16 @@ source_pos[npml+10, Ny//2, Nz//2] = source_amp
 # starting relative permittivity (random for debugging)
 eps_r   = np.random.random((Nx, Ny, Nz)) + 1
 F = fdtd(eps_r, dL=dL, npml=pml)
+dt = F.dt
 
-steps = int(total_time / F.dt)
+steps = int(total_time / dt)
 print('{} time steps'.format(steps))
 
-gaussian = lambda t: source_pos * source_amp * np.exp(-(t - t0 / F.dt)**2 / 2 / (sigma / F.dt)**2)
-source = lambda t: source_pos * gaussian(t) * np.cos(omega * t * F.dt)
+gaussian = lambda t: source_pos * source_amp * np.exp(-(t - t0 / dt)**2 / 2 / (sigma / dt)**2)
+source = lambda t: source_pos * gaussian(t) * np.cos(omega * t * dt)
 
-plt.plot(F.dt * np.arange(steps), np.sum(source(np.arange(steps)), axis=(0,1)))
-plt.show()
+# plt.plot(dt * np.arange(steps), np.sum(source(np.arange(steps)), axis=(0,1)))
+# plt.show()
 
 measure_pos = np.zeros((Nx, Ny, Nz))
 measure_pos[-npml-10, Ny//2, Nz//2] = 1
@@ -60,9 +79,8 @@ def objective(eps_space):
     for t_index in range(steps):
         fields = F.forward(Jz=source(t_index))
         measured.append(npa.sum(fields['Ez'] * measure_pos))
-    # measured_f = npa.fft.fft(npa.array(measured))
-    # measured_f = npa.fft.fftshift(measured_f)
-    measured_f = dft(steps) @ npa.array(measured)
+    measured_f = my_fft(npa.array(measured))
+    # measured_f = dft(steps) @ npa.array(measured)
     spectral_power = npa.square(npa.abs(measured_f))
     return spectral_power
 
@@ -70,14 +88,14 @@ eps_space = 1.0
 spectral_power = objective(eps_space)
 jac_power = jacobian(objective, mode='forward')(eps_space)
 
-N = steps
+n_disp = 140
 
 fig, ax1 = plt.subplots()
 ax2 = ax1.twinx()
-delta_f = 1/steps/F.dt
-freq_x = np.arange(steps) * delta_f
-ax1.plot(freq_x, spectral_power[:N], 'k-')
-ax2.plot(freq_x, jac_power[:N,0], 'g-')
+delta_f = 1 / steps / dt
+freq_x = np.arange(n_disp) * delta_f
+ax1.plot(freq_x, spectral_power[:n_disp], 'k-')
+ax2.plot(freq_x, jac_power[:n_disp,0], 'g-', label='FMD')
 ax1.set_ylabel('spectral power', color='k')
 ax2.set_ylabel('dP/depsilon', color='g')
 ax2.spines['right'].set_color('g')
