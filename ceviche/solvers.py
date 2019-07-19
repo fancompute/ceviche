34466727a57cdb7607123fa078af4b632b1f4543
twhughes <tylerwhughes91@gmail.com>
2019-07-19 10:05:57 -0700

    testing iterative solvers on FDFD matrices

--- ceviche/solvers.py
+++ ceviche/solvers.py
@@ -1,6 +1,7 @@
 import scipy.sparse.linalg as spl
 
 # for reference https://docs.scipy.org/doc/scipy/reference/sparse.linalg.html
+# run this file with `python -W ignore ceviche/solvers.py` to suppress warnings
 
 def solve_direct(A, b):
     """ Solve Ax=b for x using a direct solver """
@@ -19,7 +20,7 @@ ITERATIVE_METHODS = {
     'cgs': spl.cgs,
     'gmres': spl.gmres,
     'lgmres': spl.lgmres,
-    'minres': spl.minres,
+    # 'minres': spl.minres,  # requires a symmetric matrix
     'qmr': spl.qmr,
     'gcrotmk': spl.gcrotmk
 }
@@ -42,23 +43,26 @@ SOLVER_MAP = {
 }
 
 if __name__ == '__main__':
+
     from scipy.sparse import csr_matrix
     from scipy.sparse import random as random_sp
     from numpy.random import random as random
+    import numpy as np
     from time import time
 
-    N = 100       # dimension of the x, and b vectors
+    N = 200       # dimension of the x, and b vectors
     density = 0.3  # sparsity of the dense matrix
     A = csr_matrix(random_sp(N, N, density=density))
-    b = random((N,))
+    b = np.random.random((N, 1)) - 0.5
 
-    print('for N = {} and density = {}'.format(N, density))
+    print('\nWITH RANDOM MATRICES:\n')
+    print('\tfor N = {} and density = {}\n'.format(N, density))
 
     # DIRECT SOLVE
     t0 = time()
     x = solve_direct(A, b)
     t1 = time()
-    print('direct solver:\n\ttook {} seconds'.format(t1 - t0))
+    print('\tdirect solver:\n\ttook {} seconds\n'.format(t1 - t0))
 
     # ITERATIVE SOLVES
 
@@ -66,7 +70,40 @@ if __name__ == '__main__':
         t0 = time()
         x = solve_iterative(A, b, method=method)
         t1 = time()
-        print('iterative ({}) solver:\n\ttook {} seconds'.format(method, t1 - t0))
+        print('\t\titerative ({}) solver:\n\ttook {} seconds'.format(method, t1 - t0))
 
+    print('\n')
 
+    print('WITH FDFD MATRICES:\n')
 
+    m, n = 100, 100
+    print('\tfor dimensions = {}\n'.format((m, n)))
+    eps_r = np.random.random((m, n)) + 1
+    b = np.random.random((m * n, )) - 0.5
+
+    import sys
+    sys.path.append('../ceviche')
+    from ceviche.fdfd import fdfd_ez as fdfd
+    from ceviche.constants import *
+
+    npml = 10
+    dl = 2e-8
+    lambda0 = 1550e-9
+    omega0 = 2 * np.pi * C_0 / lambda0
+
+    F = fdfd(omega0, dl, eps_r, [10, 0])
+    A = F.A
+
+    # DIRECT SOLVE
+    t0 = time()
+    x = solve_direct(A, b)
+    t1 = time()
+    print('\tdirect solver:\n\ttook {} seconds\n'.format(t1 - t0))
+
+    # ITERATIVE SOLVES
+
+    for method in ITERATIVE_METHODS.keys():
+        t0 = time()
+        x = solve_iterative(A, b, method=method)
+        t1 = time()
+        print('\t\titerative ({}) solver:\n\ttook {} seconds'.format(method, t1 - t0))
