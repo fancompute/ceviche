Fix numerical instability in gradient test

tests/test_gradients_fdfd.py
@@ -45,8 +45,8 @@ class TestFDFD(unittest.TestCase):
         self.source_mask[15, 15] = 1
 
         # sources (chosen to have objectives around 1)
-        self.source_amp_ez = 1e-8
-        self.source_amp_hz = 1e-8
+        self.source_amp_ez = 1.0
+        self.source_amp_hz = 1.0
 
         self.source_ez = np.zeros((self.Nx, self.Ny))
         self.source_ez[self.Nx//2, self.Ny//2] = self.source_amp_ez