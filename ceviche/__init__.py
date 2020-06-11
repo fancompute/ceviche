8e3bf6b42140205f7e1fb821339ba080f8724a20
Kaipeng Liu <44470167+kaipengliuHIT@users.noreply.github.com>
2020-06-11 11:48:15 +0800

    Update __init__.py
    add fdfd3D here

--- ceviche/__init__.py
+++ ceviche/__init__.py
@@ -4,9 +4,10 @@ name = "ceviche"
 __version__ = '0.1.1'
 
 from .fdtd import fdtd
-from .fdfd import fdfd_ez, fdfd_hz
+from .fdfd import fdfd_ez, fdfd_hz, fdfd3D
 from .jacobians import jacobian
 
 from . import viz
 from . import modes
 from . import utils
+
